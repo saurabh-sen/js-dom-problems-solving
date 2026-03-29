b032134fd0421a3a50c645cab5b9523df328d447
saurabh.sen <saurabh.sen@wavemaker.com>
2026-03-29 15:48:38 +0530

    feat: Implement flattenObject function to recursively flatten nested objects and arrays into a single-level object.

--- problemSolving/flattenObject/index.ts
+++ problemSolving/flattenObject/index.ts
@@ -1,10 +1,32 @@
 console.log('Problem Solving - Flatten Object');
 
 interface IFlattenParam {
-    [key:string] : string | number | Array<number> | IFlattenParam 
+    [key: string]: string | number | Array<number> | IFlattenParam
 }
 
-const flatten= (obj: IFlattenParam) => {}
+const flatten = (obj: IFlattenParam) => {
+    const result = {};
+    const dfs = (obj, prevPath) => {
+        Object.keys(obj).forEach(item => {
+            const value = obj[item];
+            const key = prevPath ? prevPath + '.' + item : item;
+            if (Array.isArray(value)) {
+                value.forEach((item, i) => {
+                    const copykey = key + '.' + i;
+                    if (typeof item !== 'object') {
+                        result[copykey] = item;
+                    } else dfs(item, copykey)
+                })
+            } else if (typeof value == 'object') {
+                dfs(value, key);
+            } else {
+                result[key] = value;
+            }
+        })
+    }
+    dfs(obj, '');
+    return result;
+}
 
 const nested = {
     A: "12",
